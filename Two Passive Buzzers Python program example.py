ddb5d00d93c59e4a4de9364cf0fad438f80ce8fd
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2024-10-08 00:44:09 -0400

    Update Two Passive Buzzers Python program example.py

--- Two Passive Buzzers Python program example.py
+++ Two Passive Buzzers Python program example.py
@@ -56,27 +56,25 @@ passbuzz1=GPIO.PWM(passbuzz_pin[0],400)
 passbuzz2=GPIO.PWM(passbuzz_pin[1],400)
 
 passbuzz1.start(50)
-
-for i in range(1):        
-    for x in range(50,300):
-        passbuzz1.ChangeFrequency(x)
-        time.sleep(.004)
-    for x in range(300,50,-1):
-        passbuzz1.ChangeFrequency(x)
-        time.sleep(.004)
         
+for x in range(50,300):
+    passbuzz1.ChangeFrequency(x)
+    time.sleep(.004)
+for x in range(300,50,-1):
+    passbuzz1.ChangeFrequency(x)
+    time.sleep(.004)
+    
 passbuzz1.stop(50)
 
 passbuzz2.start(50)
 
-for i in range(1):
-    for x in range(50,300):
-        passbuzz2.ChangeFrequency(x)
-        time.sleep(.004)
-    for x in range(300,50,-1):
-        passbuzz2.ChangeFrequency(x)
-        time.sleep(.004)
-        
+for x in range(50,300):
+    passbuzz2.ChangeFrequency(x)
+    time.sleep(.004)
+for x in range(300,50,-1):
+    passbuzz2.ChangeFrequency(x)
+    time.sleep(.004)
+    
 passbuzz2.stop(50)
 
 GPIO.cleanup()
